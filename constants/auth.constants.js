e6515a5be70798e4d47385a5b9f3c5b2aa4f5a34
Nicola Giuseppe Ninni <nicolagiuseppe.ninni@gmail.com>
2026-02-08 22:10:08 +0100

    Added app constants and middlewares

---
+++ constants/auth.constants.js
@@ -0,0 +1,4 @@
+export const AUTH_CODE_EXPIRATION_MS = 600000; // 10 minuti
+export const ACCESS_TOKEN_EXPIRATION = '15m';
+export const REFRESH_TOKEN_EXPIRATION = '7d';
+export const REFRESH_TOKEN_EXPIRATION_MS = 7 * 24 * 60 * 60 * 1000;
