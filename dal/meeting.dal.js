19b913de1ea856017fd4fe7a467692f0866d9287
Nicola Giuseppe Ninni <nicolagiuseppe.ninni@gmail.com>
2026-02-08 22:07:00 +0100

    Added data access layers to lose coupling

---
+++ dal/meeting.dal.js
@@ -0,0 +1,53 @@
+import Meeting from '../models/Meeting.model.js';
+
+export const findById = async (id) => {
+  return Meeting.findById(id);
+};
+
+export const findByDateRange = async (rangeStart, rangeEnd) => {
+  return Meeting.find({
+    startDateTime: { $lte: rangeEnd },
+    endDateTime: { $gte: rangeStart }
+  });
+};
+
+export const findByParticipant = async (userId) => {
+  return Meeting.find({ 'participants.user': userId });
+};
+
+export const createMeeting = async (meetingData) => {
+  const meeting = new Meeting(meetingData);
+  return meeting.save();
+};
+
+export const updateMeeting = async (id, updateData) => {
+  return Meeting.findByIdAndUpdate(id, updateData, { new: true });
+};
+
+export const deleteMeeting = async (id) => {
+  return Meeting.findByIdAndDelete(id);
+};
+
+export const addParticipant = async (meetingId, userId, defaultStatus = 'maybe') => {
+  return Meeting.findByIdAndUpdate(
+    meetingId,
+    { $push: { participants: { user: userId, defaultStatus } } },
+    { new: true }
+  );
+};
+
+export const removeParticipant = async (meetingId, userId) => {
+  return Meeting.findByIdAndUpdate(
+    meetingId,
+    { $pull: { participants: { user: userId } } },
+    { new: true }
+  );
+};
+
+export const updateParticipantDefaultStatus = async (meetingId, userId, defaultStatus) => {
+  return Meeting.findOneAndUpdate(
+    { _id: meetingId, 'participants.user': userId },
+    { $set: { 'participants.$.defaultStatus': defaultStatus } },
+    { new: true }
+  );
+};
